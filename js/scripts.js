9ed559465a95ac9346c8983ce53e43a6ce763a16
Kelly Young <kkristineyoung@gmail.com>
2021-01-22 15:55:25 -0500

    Adds variables (javascript/jquery) to save form data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+  $("form#survey-form").submit(function(event) {
+    event.preventDefault();
+    const name = $("input#name").val();
+    const email = $("input#email").val();
+    const number = $("input#number").val();
+    const experience = $("#experience").val();
+    let selectedProducts = [];
+    $("input:checkbox[name=product]:checked").each(function() {
+      const selectedProduct = $(this).val();
+      selectedProducts.push(selectedProduct);
+    });
+    const comments = $("#comments").val();  
+    $("form#survey-form").hide();
+    $("div.form-submitted").show();
+  });
 
 });
